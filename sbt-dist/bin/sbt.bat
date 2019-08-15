47d1e81bb72d778f0e8dedd84815fc054df2fe53
aw84 <silencewalker@gmail.com>
2019-08-15 14:52:10 +0200

    Use env JAVA_HOME to determine which Java to run

--- sbt-dist/bin/sbt.bat
+++ sbt-dist/bin/sbt.bat
@@ -1,7 +1,9 @@
 @echo off
 set SBT_HOME=%~dp0
-java -cp "%SBT_HOME%sbt-launch-1.2.8.jar" xsbt.boot.Boot %*
+%JAVA_HOME%\bin\java.exe -cp "%SBT_HOME%sbt-launch-1.2.8.jar" xsbt.boot.Boot %*
 exit /b 1
+
+
 @REM SBT launcher script
 @REM 
 @REM Envioronment:
